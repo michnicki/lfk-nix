92f8ac2eee7d7000a9d337df333b5eb71fabaeef
Thomas Michnicki <thomas.michnicki@gmail.com>
2026-07-12 08:06:02 +0200

    chore: bump lfk to 0.15.6

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      version = "0.14.19";
+      version = "0.15.6";
     in
     {
       packages = forAllSystems (system:
@@ -25,7 +25,7 @@
               owner = "janosmiko";
               repo = "lfk";
               rev = "v${version}";
-              hash = "sha256-57bAYTU6/Fv7jbKK+RAbITECl4hYQzS2kkn7bytnW+Y=";
+              hash = "sha256-PtxMaCCEgJPBhpsLIQfKukoal1GLi6xkXUgnPXZPbcU=";
             };
 
             vendorHash = "sha256-WYx/eMArAsiyfEvrBZmTfK2ABxX2X1VQEtDCxC+UyP8=";
